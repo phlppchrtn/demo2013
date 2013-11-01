ac81d2ede2ad2add3efe8381df546a7b347ba977
pchretien@kleegroup.com <pchretien55@gmail.com>
2013-11-01 16:23:41 +0100

    SOCKET

--- src/main/java/socket/tcp/KnockKnockClient.java
+++ src/main/java/socket/tcp/KnockKnockClient.java
@@ -1,4 +1,5 @@
 package socket.tcp;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,19 +8,22 @@ import java.net.Socket;
 
 public class KnockKnockClient {
 	public static void main(String[] args) throws IOException {
+		//Ouverture de socket
+		try (Socket socket = new Socket("localhost", 4444)) {
+			//
+			try (PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
 
-		try (Socket kkSocket = new Socket("localhost", 4444)) {
-			try (PrintWriter out = new PrintWriter(kkSocket.getOutputStream(), true)) {
-				try (BufferedReader in = new BufferedReader(new InputStreamReader(kkSocket.getInputStream()))) {
+				try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+
 					try (BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))) {
 						String fromServer;
 						String fromUser;
 
 						while ((fromServer = in.readLine()) != null) {
 							System.out.println("Server: " + fromServer);
-							if (fromServer.equals("Bye."))
+							if (fromServer.toUpperCase().startsWith("BYE")) {
 								break;
-
+							}
 							fromUser = stdIn.readLine();
 							if (fromUser != null) {
 								System.out.println("Client: " + fromUser);

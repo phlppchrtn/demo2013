0d391bb5b92e4011dfb8734fb65580c9ba0f9188
Philippe Chrétien <pchretien55@gmail.com>
2014-08-18 18:22:08 +0200

    move to vertigo-labs

--- src/main/java/io/vertigo/nitro/tcp/io/resp/RespCommandHandler.java
+++
@@ -1,8 +0,0 @@
-package io.vertigo.nitro.tcp.io.resp;
-
-import java.io.IOException;
-import java.io.OutputStream;
-
-public interface RespCommandHandler {
-	void onCommand(OutputStream out, RespCommand command) throws IOException;
-}
